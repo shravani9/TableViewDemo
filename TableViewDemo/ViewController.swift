102744a12a721151432f5dc11f51b81734c2bda8
shravani.deshpande <shravani.deshpande@senecaglobal.com>
2021-02-03 12:32:46 +0530

    Initial Commit

---
+++ TableViewDemo/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  TableViewDemo
+//
+//  Created by Shravani on 03/02/21.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
